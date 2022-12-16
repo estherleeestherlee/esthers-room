7a986d314cf0bd4f02f2908f3d27231390b0a04e
estherleeestherlee <112660757+estherleeestherlee@users.noreply.github.com>
2022-12-15 20:54:32 -0500

    .

--- scripts.js
+++ scripts.js
@@ -89,3 +89,24 @@ $('a').hover(
     $('body').removeClass('cursor')
   }
 );
+
+
+$(document).ready(function(){
+    generateBinary();
+      setInterval('updateBinary()', 5);
+  });
+  
+  function generateBinary(){
+    str = "";
+    for ( var i = 0; i < 3500; i++ ) {
+      str = str + Math.round(Math.random());
+    }
+    $(".bin").html(str);
+  }
+  
+  function updateBinary() {
+    str = $(".bin").html();
+    n = str.length;
+    r = Math.floor(Math.random() * n + 1)
+    $(".bin").html(str.substring(0, r) + Math.round(Math.random()) + str.substring(r + 1));
+  }
